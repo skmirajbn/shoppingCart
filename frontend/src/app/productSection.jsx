0573cef424b04325592e1dfcb375040997d89197
skmirajbn <skmirajbn@gmail.com>
2023-11-15 23:17:13 +0600

    updated

--- frontend/src/app/productSection.jsx
+++ frontend/src/app/productSection.jsx
@@ -1,145 +1,22 @@
 /* eslint-disable @next/next/no-img-element */
 
-import Ratings from "@/components/ratings";
-import { CartContext } from "@/context/cartContext";
-import { useContext } from "react";
-import { ToastContainer, toast } from "react-toastify";
+import Product from "@/components/product";
 import "react-toastify/dist/ReactToastify.css";
 
 export default function ProductSection() {
-  const { cart, setCart } = useContext(CartContext);
-  const addToCart = async () => {
-    try {
-      const resolveAfter3Sec = new Promise((resolve) => setTimeout(resolve, 1000));
-      await toast.promise(resolveAfter3Sec, {
-        pending: "Adding to the Cart",
-        success: "Added to cart",
-        error: "Cart Add Failed",
-      });
-      setCart([...cart, 1]);
-    } catch (error) {
-      // Handle any errors here
-      console.error(error);
-    }
-  };
   return (
     <section className="px-6 py-4 mx-auto bg-gray-100 max-w-7xl">
-      <ToastContainer position="top-center" />
       <h2 className="py-6 text-4xl font-bold text-center">Product List</h2>
       <div className="p-10 bg-white rounded-lg">
         <div className="grid grid-cols-4 gap-4">
-          {/* Single Product */}
-          <div className="max-w-xs space-y-1 overflow-hidden bg-gray-100 rounded-lg">
-            <img className="object-cover w-full h-72" src="images/productImages/running-shoes-2048px-9718.jpg" alt="" />
-            <div className="p-4">
-              <h3 className="text-lg font-semibold line-clamp-2">New Premium Men&apos;s Neaker For Party or Offices</h3>
-              <h5 className="text-lg font-bold text-emerald-500">
-                <i class="fa-solid fa-bangladeshi-taka-sign"></i> 5000
-              </h5>
-              <Ratings className="text-orange-600" rating={4} />
-              <button className="px-6 py-2 mt-4 text-white rounded-lg bg-emerald-600 hover:bg-emerald-700" onClick={addToCart}>
-                Add to Cart <i class="ml-2 fa-solid fa-cart-plus"></i>
-              </button>
-            </div>
-          </div>
-          {/* Single Product */}
-          <div className="max-w-xs space-y-1 overflow-hidden bg-gray-100 rounded-lg">
-            <img className="object-cover w-full h-72" src="images/productImages/running-shoes-2048px-9718.jpg" alt="" />
-            <div className="p-4">
-              <h3 className="text-lg font-semibold line-clamp-2">New Premium Men&apos;s Neaker For Party or Offices</h3>
-              <h5 className="text-lg font-bold text-emerald-500">
-                <i class="fa-solid fa-bangladeshi-taka-sign"></i> 5000
-              </h5>
-              <Ratings className="text-orange-600" rating={4} />
-              <button className="px-6 py-2 mt-4 text-white rounded-lg bg-emerald-600">
-                Add to Cart <i class="ml-2 fa-solid fa-cart-plus"></i>
-              </button>
-            </div>
-          </div>
-          {/* Single Product */}
-          <div className="max-w-xs space-y-1 overflow-hidden bg-gray-100 rounded-lg">
-            <img className="object-cover w-full h-72" src="images/productImages/running-shoes-2048px-9718.jpg" alt="" />
-            <div className="p-4">
-              <h3 className="text-lg font-semibold line-clamp-2">New Premium Men&apos;s Neaker For Party or Offices</h3>
-              <h5 className="text-lg font-bold text-emerald-500">
-                <i class="fa-solid fa-bangladeshi-taka-sign"></i> 5000
-              </h5>
-              <Ratings className="text-orange-600" rating={4} />
-              <button className="px-6 py-2 mt-4 text-white rounded-lg bg-emerald-600">
-                Add to Cart <i class="ml-2 fa-solid fa-cart-plus"></i>
-              </button>
-            </div>
-          </div>
-          {/* Single Product */}
-          <div className="max-w-xs space-y-1 overflow-hidden bg-gray-100 rounded-lg">
-            <img className="object-cover w-full h-72" src="images/productImages/running-shoes-2048px-9718.jpg" alt="" />
-            <div className="p-4">
-              <h3 className="text-lg font-semibold line-clamp-2">New Premium Men&apos;s Neaker For Party or Offices</h3>
-              <h5 className="text-lg font-bold text-emerald-500">
-                <i class="fa-solid fa-bangladeshi-taka-sign"></i> 5000
-              </h5>
-              <Ratings className="text-orange-600" rating={4} />
-              <button className="px-6 py-2 mt-4 text-white rounded-lg bg-emerald-600 hover:bg-emerald-600">
-                Add to Cart <i class="ml-2 fa-solid fa-cart-plus"></i>
-              </button>
-            </div>
-          </div>
-          {/* Single Product */}
-          <div className="max-w-xs space-y-1 overflow-hidden bg-gray-100 rounded-lg">
-            <img className="object-cover w-full h-72" src="images/productImages/running-shoes-2048px-9718.jpg" alt="" />
-            <div className="p-4">
-              <h3 className="text-lg font-semibold line-clamp-2">New Premium Men&apos;s Neaker For Party or Offices</h3>
-              <h5 className="text-lg font-bold text-emerald-500">
-                <i class="fa-solid fa-bangladeshi-taka-sign"></i> 5000
-              </h5>
-              <Ratings className="text-orange-600" rating={4} />
-              <button className="px-6 py-2 mt-4 text-white rounded-lg bg-emerald-600">
-                Add to Cart <i class="ml-2 fa-solid fa-cart-plus"></i>
-              </button>
-            </div>
-          </div>
-          {/* Single Product */}
-          <div className="max-w-xs space-y-1 overflow-hidden bg-gray-100 rounded-lg">
-            <img className="object-cover w-full h-72" src="images/productImages/running-shoes-2048px-9718.jpg" alt="" />
-            <div className="p-4">
-              <h3 className="text-lg font-semibold line-clamp-2">New Premium Men&apos;s Neaker For Party or Offices</h3>
-              <h5 className="text-lg font-bold text-emerald-500">
-                <i class="fa-solid fa-bangladeshi-taka-sign"></i> 5000
-              </h5>
-              <Ratings className="text-orange-600" rating={4} />
-              <button className="px-6 py-2 mt-4 text-white rounded-lg bg-emerald-600">
-                Add to Cart <i class="ml-2 fa-solid fa-cart-plus"></i>
-              </button>
-            </div>
-          </div>
-          {/* Single Product */}
-          <div className="max-w-xs space-y-1 overflow-hidden bg-gray-100 rounded-lg">
-            <img className="object-cover w-full h-72" src="images/productImages/running-shoes-2048px-9718.jpg" alt="" />
-            <div className="p-4">
-              <h3 className="text-lg font-semibold line-clamp-2">New Premium Men&apos;s Neaker For Party or Offices</h3>
-              <h5 className="text-lg font-bold text-emerald-500">
-                <i class="fa-solid fa-bangladeshi-taka-sign"></i> 5000
-              </h5>
-              <Ratings className="text-orange-600" rating={4} />
-              <button className="px-6 py-2 mt-4 text-white rounded-lg bg-emerald-600">
-                Add to Cart <i class="ml-2 fa-solid fa-cart-plus"></i>
-              </button>
-            </div>
-          </div>
-          {/* Single Product */}
-          <div className="max-w-xs space-y-1 overflow-hidden bg-gray-100 rounded-lg">
-            <img className="object-cover w-full h-72" src="images/productImages/running-shoes-2048px-9718.jpg" alt="" />
-            <div className="p-4">
-              <h3 className="text-lg font-semibold line-clamp-2">New Premium Men&apos;s Neaker For Party or Offices</h3>
-              <h5 className="text-lg font-bold text-emerald-500">
-                <i class="fa-solid fa-bangladeshi-taka-sign"></i> 5000
-              </h5>
-              <Ratings className="text-orange-600" rating={4} />
-              <button className="px-6 py-2 mt-4 text-white rounded-lg bg-emerald-600">
-                Add to Cart <i class="ml-2 fa-solid fa-cart-plus"></i>
-              </button>
-            </div>
-          </div>
+          <Product image="images/productImages/running-shoes-2048px-9718.jpg" name="New Premium Men's Neaker For Party or Offices" price="5000" rating={4} />
+          <Product image="images/productImages/running-shoes-2048px-9718.jpg" name="Trendy Shoes for men and women. Luxary low price shoes only" price="5000" rating={2} />
+          <Product image="images/productImages/running-shoes-2048px-9718.jpg" name="Trendy Shoes for men and women. Luxary low price shoes only" price="5000" rating={2} />
+          <Product image="images/productImages/running-shoes-2048px-9718.jpg" name="Trendy Shoes for men and women. Luxary low price shoes only" price="5000" rating={2} />
+          <Product image="images/productImages/running-shoes-2048px-9718.jpg" name="Trendy Shoes for men and women. Luxary low price shoes only" price="5000" rating={2} />
+          <Product image="images/productImages/running-shoes-2048px-9718.jpg" name="Trendy Shoes for men and women. Luxary low price shoes only" price="5000" rating={2} />
+          <Product image="images/productImages/running-shoes-2048px-9718.jpg" name="Trendy Shoes for men and women. Luxary low price shoes only" price="5000" rating={2} />
+          <Product image="images/productImages/running-shoes-2048px-9718.jpg" name="Trendy Shoes for men and women. Luxary low price shoes only" price="5000" rating={2} />
         </div>
       </div>
     </section>
---
+++ frontend/src/components/product.jsx
@@ -0,0 +1,41 @@
+/* eslint-disable @next/next/no-img-element */
+import { CartContext } from "@/context/cartContext";
+import { useContext } from "react";
+import { ToastContainer, toast } from "react-toastify";
+import Ratings from "./ratings";
+
+export default function Product({ name, price, rating, image }) {
+  const { cart, setCart } = useContext(CartContext);
+  const addToCart = async () => {
+    try {
+      const resolveAfter3Sec = new Promise((resolve) => setTimeout(resolve, 1000));
+      await toast.promise(resolveAfter3Sec, {
+        pending: "Adding to the Cart",
+        success: "Added to cart",
+        error: "Cart Add Failed",
+      });
+      setCart([...cart, 1]);
+    } catch (error) {
+      // Handle any errors here
+      console.error(error);
+    }
+  };
+  return (
+    <div className="h-full max-w-xs space-y-1 overflow-hidden bg-gray-100 rounded-lg">
+      <img className="object-cover w-full h-72" src="images/productImages/running-shoes-2048px-9718.jpg" alt="" />
+      <div className="flex flex-col justify-between h-[12.5rem] p-4">
+        <h3 className="text-lg font-semibold line-clamp-2">{name}</h3>
+        <div>
+          <h5 className="text-lg font-bold text-emerald-500">
+            <i class="fa-solid fa-bangladeshi-taka-sign"></i> {price}
+          </h5>
+          <Ratings className="text-orange-600" rating={rating} />
+          <button className="px-6 py-2 mt-4 text-white rounded-lg bg-emerald-600 hover:bg-emerald-700" onClick={addToCart}>
+            Add to Cart <i class="ml-2 fa-solid fa-cart-plus"></i>
+          </button>
+        </div>
+      </div>
+      <ToastContainer position="top-center" />
+    </div>
+  );
+}
