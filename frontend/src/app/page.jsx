9166b864a95c1dbdac8b0fc124bdf3884659f2c0
skmirajbn <skmirajbn@gmail.com>
2023-11-15 21:45:11 +0600

    updated

--- frontend/src/app/page.jsx
+++ frontend/src/app/page.jsx
@@ -1,3 +1,126 @@
+import Ratings from "@/components/ratings";
+
+/* eslint-disable @next/next/no-img-element */
 export default function Home() {
-  return <main className="">This is body</main>;
+  return (
+    <section className="px-6 py-4 mx-auto bg-gray-100 max-w-7xl">
+      <h2 className="py-6 text-4xl font-bold text-center">Product List</h2>
+      <div className="p-10 bg-white rounded-lg">
+        <div className="grid grid-cols-4 gap-4">
+          {/* Single Product */}
+          <div className="max-w-xs space-y-1 overflow-hidden bg-gray-100 rounded-lg">
+            <img className="object-cover w-full h-72" src="images/productImages/running-shoes-2048px-9718.jpg" alt="" />
+            <div className="p-4">
+              <h3 className="text-lg font-semibold line-clamp-2">New Premium Men&apos;s Neaker For Party or Offices</h3>
+              <h5 className="text-lg font-bold text-emerald-500">
+                <i class="fa-solid fa-bangladeshi-taka-sign"></i> 5000
+              </h5>
+              <Ratings className="text-orange-600" rating={4} />
+              <button className="px-6 py-2 mt-4 text-white rounded-lg bg-emerald-600">
+                Add to Cart <i class="ml-2 fa-solid fa-cart-plus"></i>
+              </button>
+            </div>
+          </div>
+          {/* Single Product */}
+          <div className="max-w-xs space-y-1 overflow-hidden bg-gray-100 rounded-lg">
+            <img className="object-cover w-full h-72" src="images/productImages/running-shoes-2048px-9718.jpg" alt="" />
+            <div className="p-4">
+              <h3 className="text-lg font-semibold line-clamp-2">New Premium Men&apos;s Neaker For Party or Offices</h3>
+              <h5 className="text-lg font-bold text-emerald-500">
+                <i class="fa-solid fa-bangladeshi-taka-sign"></i> 5000
+              </h5>
+              <Ratings className="text-orange-600" rating={4} />
+              <button className="px-6 py-2 mt-4 text-white rounded-lg bg-emerald-600">
+                Add to Cart <i class="ml-2 fa-solid fa-cart-plus"></i>
+              </button>
+            </div>
+          </div>
+          {/* Single Product */}
+          <div className="max-w-xs space-y-1 overflow-hidden bg-gray-100 rounded-lg">
+            <img className="object-cover w-full h-72" src="images/productImages/running-shoes-2048px-9718.jpg" alt="" />
+            <div className="p-4">
+              <h3 className="text-lg font-semibold line-clamp-2">New Premium Men&apos;s Neaker For Party or Offices</h3>
+              <h5 className="text-lg font-bold text-emerald-500">
+                <i class="fa-solid fa-bangladeshi-taka-sign"></i> 5000
+              </h5>
+              <Ratings className="text-orange-600" rating={4} />
+              <button className="px-6 py-2 mt-4 text-white rounded-lg bg-emerald-600">
+                Add to Cart <i class="ml-2 fa-solid fa-cart-plus"></i>
+              </button>
+            </div>
+          </div>
+          {/* Single Product */}
+          <div className="max-w-xs space-y-1 overflow-hidden bg-gray-100 rounded-lg">
+            <img className="object-cover w-full h-72" src="images/productImages/running-shoes-2048px-9718.jpg" alt="" />
+            <div className="p-4">
+              <h3 className="text-lg font-semibold line-clamp-2">New Premium Men&apos;s Neaker For Party or Offices</h3>
+              <h5 className="text-lg font-bold text-emerald-500">
+                <i class="fa-solid fa-bangladeshi-taka-sign"></i> 5000
+              </h5>
+              <Ratings className="text-orange-600" rating={4} />
+              <button className="px-6 py-2 mt-4 text-white rounded-lg bg-emerald-600">
+                Add to Cart <i class="ml-2 fa-solid fa-cart-plus"></i>
+              </button>
+            </div>
+          </div>
+          {/* Single Product */}
+          <div className="max-w-xs space-y-1 overflow-hidden bg-gray-100 rounded-lg">
+            <img className="object-cover w-full h-72" src="images/productImages/running-shoes-2048px-9718.jpg" alt="" />
+            <div className="p-4">
+              <h3 className="text-lg font-semibold line-clamp-2">New Premium Men&apos;s Neaker For Party or Offices</h3>
+              <h5 className="text-lg font-bold text-emerald-500">
+                <i class="fa-solid fa-bangladeshi-taka-sign"></i> 5000
+              </h5>
+              <Ratings className="text-orange-600" rating={4} />
+              <button className="px-6 py-2 mt-4 text-white rounded-lg bg-emerald-600">
+                Add to Cart <i class="ml-2 fa-solid fa-cart-plus"></i>
+              </button>
+            </div>
+          </div>
+          {/* Single Product */}
+          <div className="max-w-xs space-y-1 overflow-hidden bg-gray-100 rounded-lg">
+            <img className="object-cover w-full h-72" src="images/productImages/running-shoes-2048px-9718.jpg" alt="" />
+            <div className="p-4">
+              <h3 className="text-lg font-semibold line-clamp-2">New Premium Men&apos;s Neaker For Party or Offices</h3>
+              <h5 className="text-lg font-bold text-emerald-500">
+                <i class="fa-solid fa-bangladeshi-taka-sign"></i> 5000
+              </h5>
+              <Ratings className="text-orange-600" rating={4} />
+              <button className="px-6 py-2 mt-4 text-white rounded-lg bg-emerald-600">
+                Add to Cart <i class="ml-2 fa-solid fa-cart-plus"></i>
+              </button>
+            </div>
+          </div>
+          {/* Single Product */}
+          <div className="max-w-xs space-y-1 overflow-hidden bg-gray-100 rounded-lg">
+            <img className="object-cover w-full h-72" src="images/productImages/running-shoes-2048px-9718.jpg" alt="" />
+            <div className="p-4">
+              <h3 className="text-lg font-semibold line-clamp-2">New Premium Men&apos;s Neaker For Party or Offices</h3>
+              <h5 className="text-lg font-bold text-emerald-500">
+                <i class="fa-solid fa-bangladeshi-taka-sign"></i> 5000
+              </h5>
+              <Ratings className="text-orange-600" rating={4} />
+              <button className="px-6 py-2 mt-4 text-white rounded-lg bg-emerald-600">
+                Add to Cart <i class="ml-2 fa-solid fa-cart-plus"></i>
+              </button>
+            </div>
+          </div>
+          {/* Single Product */}
+          <div className="max-w-xs space-y-1 overflow-hidden bg-gray-100 rounded-lg">
+            <img className="object-cover w-full h-72" src="images/productImages/running-shoes-2048px-9718.jpg" alt="" />
+            <div className="p-4">
+              <h3 className="text-lg font-semibold line-clamp-2">New Premium Men&apos;s Neaker For Party or Offices</h3>
+              <h5 className="text-lg font-bold text-emerald-500">
+                <i class="fa-solid fa-bangladeshi-taka-sign"></i> 5000
+              </h5>
+              <Ratings className="text-orange-600" rating={4} />
+              <button className="px-6 py-2 mt-4 text-white rounded-lg bg-emerald-600">
+                Add to Cart <i class="ml-2 fa-solid fa-cart-plus"></i>
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 }
---
+++ frontend/src/components/ratings.jsx
@@ -0,0 +1,10 @@
+export default function Rating({ rating = 4, className = "" }) {
+  const stars = [];
+  for (let i = 0; i < rating; i++) {
+    stars.push(<i className={`fa-solid fa-star ${className}`} key={i}></i>);
+  }
+  for (let i = rating; i < 5; i++) {
+    stars.push(<i className={`fa-regular fa-star ${className}`} key={i}></i>);
+  }
+  return <p>{stars}</p>;
+}
