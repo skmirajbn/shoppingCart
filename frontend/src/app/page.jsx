c92343812c5a67f7cfcc4d4521469f4a33d47022
skmirajbn <skmirajbn@gmail.com>
2023-11-16 21:48:38 +0600

    updated

--- frontend/src/app/page.jsx
+++ frontend/src/app/page.jsx
@@ -2,11 +2,18 @@
 /* eslint-disable @next/next/no-img-element */
 import Header from "@/components/header";
 import { CartContext } from "@/context/cartContext";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import ProductSection from "./productSection";
 
 export default function Home() {
-  const [cartCount, setCartCount] = useState(JSON.parse(localStorage.getItem("cart"))?.length || 0);
+  const [cartCount, setCartCount] = useState(0);
+
+  useEffect(() => {
+    let cartItems = JSON.parse(localStorage?.getItem("cart")) || [];
+    let carItemsCount = cartItems?.length;
+    setCartCount(carItemsCount);
+  }, []);
+
   return (
     <main>
       <CartContext.Provider value={{ cartCount, setCartCount }}>
