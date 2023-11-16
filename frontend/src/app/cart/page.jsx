f9084c472bb0a591883f341d99656123c8c39d21
skmirajbn <skmirajbn@gmail.com>
2023-11-17 00:02:26 +0600

    updated

--- frontend/src/app/cart/page.jsx
+++ frontend/src/app/cart/page.jsx
@@ -1,9 +1,10 @@
 /* eslint-disable react-hooks/exhaustive-deps */
 "use client";
 import { CartContext } from "@/context/cartContext";
-import { useContext, useEffect } from "react";
+import { useContext, useEffect, useState } from "react";
 
 export default function Cart() {
+  const [quantity, setQuantity] = useState(1);
   const { cartProducts, productMutate } = useContext(CartContext);
   useEffect(() => {
     productMutate();
@@ -12,14 +13,20 @@ export default function Cart() {
     <section className="px-6 py-4 mx-auto bg-gray-100 max-w-7xl">
       <h2 className="py-6 text-4xl font-bold text-center">Cart</h2>
       <div className="p-10 bg-white rounded-lg">
-        <div className="grid grid-cols-4 gap-4">
-          <div>
-            {cartProducts?.map((product, index) => (
-              <div key={index}>
-                <h3>{product.name}</h3>
+        <div className="space-y-3">
+          {cartProducts?.map((product, index) => (
+            <div className="flex items-center gap-4" key={index}>
+              <img className="object-cover w-20 h-20 rounded-md" src={product.image} alt="" />
+              <input className="w-12 px-2 py-2 border-2 border-gray-500 rounded-lg" value={1} type="number" />
+              <div>
+                <h3 className="text-lg font-semibold text-green-800">{product.name}</h3>
+                <h4 className="text-orange-500">
+                  <i class="fa-solid fa-bangladeshi-taka-sign"></i> {product.price}
+                </h4>
+                <h3>Total</h3>
               </div>
-            ))}
-          </div>
+            </div>
+          ))}
         </div>
       </div>
     </section>
