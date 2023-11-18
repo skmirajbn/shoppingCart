f2209c62651bc2d32f3fcfda1f19e02c10007d51
Sk Miraj <skmirajbn@gmail.com>
2023-11-18 11:33:00 +0600

    updated

--- frontend/src/hooks/useIsLoggedIn.js
+++ frontend/src/hooks/useIsLoggedIn.js
@@ -5,13 +5,11 @@ export const useIsLoggedIn = () => {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
   const { user } = useAuth({ middleware: "guest" });
   useEffect(() => {
-    setTimeout(() => {
-      if (user) {
-        setIsLoggedIn(true);
-      } else {
-        setIsLoggedIn(false);
-      }
-    }, 5000);
+    if (user) {
+      setIsLoggedIn(true);
+    } else {
+      setIsLoggedIn(false);
+    }
   }, [user]);
   return isLoggedIn;
 };
